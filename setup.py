8debf7f59f9545556a5212c11b28f9d473af5cc3
Oleh Danylevych <danylevych123@gmail.com>
2024-08-29 21:21:10 +0300

    Update dependencies and project configuration

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='momo',
+    version='1.0.0',
+    description='MoMo is a module that does Morphological Modeling',
+    long_description='MoMo is an open source module that does Morphological Modeling',
+    author='Oleh Danylevych',
+    author_email='danylevych123@email.com',
+    url='https://github.com/danylevych/MoMo',
+    packages=['momo'],
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
